abc1c7df76a9ae4df0eb7576400ba49e4ad51380
Wojciech Dudek <wdudek82@gmail.com>
2016-08-07 13:21:38 +0200

    Tweaking settings

--- backend/production.py
+++ backend/production.py
@@ -3,12 +3,12 @@ ALLOWED_HOSTS = ['wdudek.pythonanywhere.com', 'localhost']
 ########
 # SQLite
 ########
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 #######
 # MySQL

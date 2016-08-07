96354451181678ceaf98a961a46a8020f776656e
Wojciech Dudek <wdudek82@gmail.com>
2016-08-07 13:53:47 +0200

    Modification in routing #2

--- heart/urls.py
+++ heart/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^app1/', include('app1.urls', namespace='app1')),
-    url(r'', views.Boot.as_view(), name='boot'),
+    url(r'^$', views.Boot.as_view(), name='boot'),
 ]
 
